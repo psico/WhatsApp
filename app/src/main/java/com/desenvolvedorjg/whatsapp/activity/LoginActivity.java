89e489762be0513198cf14ea1a2b29a818b8be38
João Gabriel dos Santos Rodrigues <desenvolvedorjg@gmail.com>
2019-03-09 23:25:24 -0300

    [ADD] Implementado logout.

--- app/src/main/java/com/desenvolvedorjg/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/desenvolvedorjg/whatsapp/activity/LoginActivity.java
@@ -33,6 +33,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        verificarUsuarioLogado();
+
         email = (EditText) findViewById(R.id.edit_login_email);
         senha = (EditText) findViewById(R.id.edit_login_senha);
         botaoLogar = (Button) findViewById(R.id.bt_logar);
@@ -46,6 +48,14 @@ public class LoginActivity extends AppCompatActivity {
                 validarLogin();
             }
         });
+    }
+
+    private void verificarUsuarioLogado() {
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        if (autenticacao.getCurrentUser() != null) {
+            abrirTelaPrincipal();
+        }
     }
 
     private void validarLogin() {
--- app/src/main/java/com/desenvolvedorjg/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/desenvolvedorjg/whatsapp/activity/MainActivity.java
@@ -1,21 +1,37 @@
 package com.desenvolvedorjg.whatsapp.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.desenvolvedorjg.whatsapp.R;
 import com.desenvolvedorjg.whatsapp.config.ConfiguracaoFirebase;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button botaoSair;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoSair = (Button) findViewById(R.id.bt_sair);
+
+        botaoSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+                autenticacao.signOut();
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
