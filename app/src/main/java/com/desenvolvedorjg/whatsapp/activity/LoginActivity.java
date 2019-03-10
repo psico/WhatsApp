d0ffbc394f7308ff84e7ae8ed2757fbed6e14656
João Gabriel dos Santos Rodrigues <desenvolvedorjg@gmail.com>
2019-03-09 23:15:37 -0300

    [ADD] Implementado o login.

--- app/src/main/java/com/desenvolvedorjg/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/desenvolvedorjg/whatsapp/activity/LoginActivity.java
@@ -2,37 +2,83 @@ package com.desenvolvedorjg.whatsapp.activity;
 
 import android.Manifest;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.desenvolvedorjg.whatsapp.R;
 import com.desenvolvedorjg.whatsapp.config.ConfiguracaoFirebase;
+import com.desenvolvedorjg.whatsapp.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 
 
 public class LoginActivity extends AppCompatActivity {
 
-    private DatabaseReference referenceFirebase;
-
-    private String[] permissoesNecessarias = new String[]{
-            Manifest.permission.SEND_SMS,
-            Manifest.permission.INTERNET
-    };
+    private EditText email;
+    private EditText senha;
+    private Button botaoLogar;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        referenceFirebase = ConfiguracaoFirebase.getFirebase();
-        referenceFirebase.child("pontos").setValue("800");
+        email = (EditText) findViewById(R.id.edit_login_email);
+        senha = (EditText) findViewById(R.id.edit_login_senha);
+        botaoLogar = (Button) findViewById(R.id.bt_logar);
+
+        botaoLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usuario = new Usuario();
+                usuario.setEmail(email.getText().toString());
+                usuario.setSenha(senha.getText().toString());
+                validarLogin();
+            }
+        });
+    }
+
+    private void validarLogin() {
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            abrirTelaPrincipal();
+                                            Toast.makeText(LoginActivity.this, "Sucesso ao fazer login", Toast.LENGTH_LONG ).show();
+                                        } else {
+                                            Toast.makeText(LoginActivity.this, "Erro ao tentar fazer login", Toast.LENGTH_LONG ).show();
+                                        }
+                                    }
+                                }
+
+        );
+    }
+
+    private void abrirTelaPrincipal() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     public void abrirCadastroUsuario(View view) {
+
         Intent intent = new Intent(LoginActivity.this, CadastroUsuarioActivity.class);
         startActivity(intent);
-        finish();
+
     }
 
 }
