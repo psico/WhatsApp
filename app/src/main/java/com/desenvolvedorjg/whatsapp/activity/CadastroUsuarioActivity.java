836599197595d38d2a0d2d48225b535ab86615e1
João Gabriel dos Santos Rodrigues <desenvolvedorjg@gmail.com>
2019-03-09 22:58:26 -0300

    [ADD] Implementado tratamento de excessoes.

--- app/src/main/java/com/desenvolvedorjg/whatsapp/activity/CadastroUsuarioActivity.java
+++ app/src/main/java/com/desenvolvedorjg/whatsapp/activity/CadastroUsuarioActivity.java
@@ -2,10 +2,7 @@ package com.desenvolvedorjg.whatsapp.activity;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
