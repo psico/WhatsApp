e6524b7ed8aa938fd531ee59472fdc6fbd3a75e0
João Gabriel dos Santos Rodrigues <desenvolvedorjg@gmail.com>
2019-03-10 23:57:34 -0300

    [ADD] Adicionado toolbar e arvore de menu.

--- app/src/main/java/com/desenvolvedorjg/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/desenvolvedorjg/whatsapp/activity/MainActivity.java
@@ -3,6 +3,9 @@ package com.desenvolvedorjg.whatsapp.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,24 +17,21 @@ public class MainActivity extends AppCompatActivity {
 
     private Button botaoSair;
     private FirebaseAuth autenticacao;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botaoSair = (Button) findViewById(R.id.bt_sair);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar(toolbar);
+    }
 
-        botaoSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
-                autenticacao.signOut();
-
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
