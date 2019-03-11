df1859d46d1bfa737b311d4722531da54690733e
João Gabriel dos Santos Rodrigues <mailparajg@gmail.com>
2019-03-11 13:22:58 -0300

    [ADD] Implementado ações para toolbar e mudado cor dos botões e criado themes.

--- app/src/main/java/com/desenvolvedorjg/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/desenvolvedorjg/whatsapp/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,23 +16,46 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button botaoSair;
-    private FirebaseAuth autenticacao;
     private Toolbar toolbar;
+    private FirebaseAuth usuarioAutenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        usuarioAutenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("WhatsApp");
         setSupportActionBar(toolbar);
     }
 
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.item_sair:
+                deslogarUsuario();
+                return true;
+            case R.id.item_configuracao:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void deslogarUsuario() {
+        usuarioAutenticacao.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
