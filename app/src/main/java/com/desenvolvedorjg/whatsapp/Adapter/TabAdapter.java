8d95b16a5c047a30c206f5e6ba0985851e5c789d
João Gabriel dos Santos Rodrigues <mailparajg@gmail.com>
2019-03-20 17:39:15 -0300

    [ADD] Implementado abas

---
+++ app/src/main/java/com/desenvolvedorjg/whatsapp/Adapter/TabAdapter.java
@@ -0,0 +1,45 @@
+package com.desenvolvedorjg.whatsapp.Adapter;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.desenvolvedorjg.whatsapp.fragment.ContatosFragment;
+import com.desenvolvedorjg.whatsapp.fragment.ConversasFragment;
+
+public class TabAdapter extends FragmentStatePagerAdapter {
+
+    private String[] tituloAbas = {"CONVERSAS","CONTATOS"};
+
+    public TabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        Fragment fragment = null;
+
+        switch (i) {
+            case 0:
+                fragment = new ContatosFragment();
+                break;
+            case 1:
+                fragment = new ConversasFragment();
+                break;
+        }
+
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return tituloAbas.length;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tituloAbas[position];
+    }
+}
--- app/src/main/java/com/desenvolvedorjg/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/desenvolvedorjg/whatsapp/activity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.desenvolvedorjg.whatsapp.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,14 +13,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.desenvolvedorjg.whatsapp.Adapter.TabAdapter;
 import com.desenvolvedorjg.whatsapp.R;
 import com.desenvolvedorjg.whatsapp.config.ConfiguracaoFirebase;
+import com.desenvolvedorjg.whatsapp.helper.SlidingTabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private FirebaseAuth usuarioAutenticacao;
+
+    private SlidingTabLayout slidingTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,19 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("WhatsApp");
         setSupportActionBar(toolbar);
+
+        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.stl_tabs);
+        viewPager = (ViewPager) findViewById(R.id.vp_pagina);
+
+        //Configurando sliding tabs
+        slidingTabLayout.setDistributeEvenly(true);
+        slidingTabLayout.setSelectedIndicatorColors(ContextCompat.getColor(this, R.color.colorAccent));
+
+        //Configurar adapter
+        TabAdapter tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        slidingTabLayout.setViewPager( viewPager);
     }
 
     @Override
